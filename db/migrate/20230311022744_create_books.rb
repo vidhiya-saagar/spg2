2f2136897ca0d650326a87a7ce6c76dc935b4e4e
Harsimran Bhinder <bhinderrr@gmail.com>
2023-03-10 22:04:42 -0500

    adding null contrainst to columns

--- db/migrate/20230311022744_create_books.rb
+++ db/migrate/20230311022744_create_books.rb
@@ -1,8 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-      t.integer :sequence
-      t.string :title
+      t.integer :sequence, :null => false
+      t.string :title, :null => false
       t.string :en_title
       t.string :en_short_summary
       t.text :en_synopsis
