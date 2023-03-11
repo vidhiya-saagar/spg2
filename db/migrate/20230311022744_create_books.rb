296f36209847fedad661ebd9038eb2005c516cdd
Harsimran Bhinder <bhinderrr@gmail.com>
2023-03-10 21:53:17 -0500

    deleted the extra books file

--- db/migrate/20230311022744_create_books.rb
+++ db/migrate/20230311022744_create_books.rb
@@ -1,6 +1,12 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
+      t.integer :sequence
+      t.string :title
+      t.string :en_title
+      t.string :en_short_summary
+      t.text :en_synopsis
+      t.text :artwork_url
 
       t.timestamps
     end
