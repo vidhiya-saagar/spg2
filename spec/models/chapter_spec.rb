7f0deedce93d18423ec3124e89b4ba124fe7c7b7
Dilraj Somel <dsomel21@gmail.com>
2023-03-12 17:55:32 -0400

    Write some basic specs

--- spec/models/chapter_spec.rb
+++ spec/models/chapter_spec.rb
@@ -14,24 +14,19 @@ RSpec.describe 'Chapter' do
     end
 
     it 'cannot save when missing `book_id`, `title`, and `number`' do
-      expect { Chapter.create!(unrequired_chapter_fields) }.to raise_error(ActiveRecord::RecordInvalid)
+      expect { Chapter.create!(unrequired_chapter_fields) }.to raise_error(ActiveRecord::NotNullViolation)
     end
 
     it 'cannot save when missing `book_id` and `title`' do
       expect do
         Chapter.create!(unrequired_chapter_fields.merge({ :number => 2 }))
-      end.to raise_error(ActiveRecord::RecordInvalid)
+      end.to raise_error(ActiveRecord::NotNullViolation)
     end
 
     it 'cannot save when `book_id` does not belong to a real book' do
       expect do
         Chapter.create!({ :book_id => 1, :number => 2, :title => 'Mangal' })
-      end.to raise_error(/Book must exist/)
-    end
-  end
-
-  context 'Preventing duplicate chapter numbers' do
-    it 'allow creation of duplicate chapter 1s, if they are for different books' do
+      end.to raise_error(ActiveRecord::InvalidForeignKey)
     end
   end
 end
