8851d7b34490335540e1f9a150c894ce1222c3af
Dilraj <dsomel21@gmail.com>
2023-03-12 20:24:37 -0400

    Ignore Rubocop in `rails_helper.rb`

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Rails/FilePath
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -63,3 +65,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+# rubocop:enable Rails/FilePath
