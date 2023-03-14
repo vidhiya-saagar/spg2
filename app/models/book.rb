bba0481cddbffaaa47c351f245fc29afa8c33860
Dilraj Somel <dsomel21@gmail.com>
2023-03-13 23:16:54 -0400

    Apply Rubocop fixes. Refactor `chapter_spec`

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
 class Book < ApplicationRecord
-  has_many :chapters
 end
-
--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class Chapter < ApplicationRecord
-  belongs_to :book
 end
