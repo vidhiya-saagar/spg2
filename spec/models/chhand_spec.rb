2630e1d9f6423c0cd8f6d7d4ac1ee7ce79472edd
Dilraj Somel <dsomel21@gmail.com>
2023-04-07 06:02:53 -0400

    Update the "code stuff" to be be in backticks

--- spec/models/chhand_spec.rb
+++ spec/models/chhand_spec.rb
@@ -8,44 +8,44 @@ RSpec.describe Chhand do
 
   describe 'validations' do
     context 'when given valid attributes' do
-      it 'saves the chhand' do
+      it 'saves the `Chhand`' do
         chhand = Chhand.new(:chhand_type => chhand_type, :chapter => chapter, :vaak => 'Example vaak')
         expect(chhand).to be_valid
       end
 
-      it 'saves the chhand without vaak' do
+      it 'saves the `Chhand` without `vaak`' do
         chhand = Chhand.new(:chhand_type => chhand_type, :chapter => chapter)
         expect(chhand).to be_valid
       end
     end
 
     context 'when given invalid attributes' do
-      it 'does not save the chhand without a chhand_type' do
+      it 'does not save the `Chhand` without a `chhand_type`' do
         chhand = Chhand.new(:chapter => chapter, :vaak => 'Example vaak')
         expect(chhand).not_to be_valid
       end
 
-      it 'does not save the chhand without a chapter' do
+      it 'does not save the `Chhand` without a `chapter`' do
         chhand = Chhand.new(:chhand_type => chhand_type, :vaak => 'Example vaak')
         expect(chhand).not_to be_valid
       end
 
-      it 'does not save the chhand with a non-existent chhand_type' do
+      it 'does not save the `Chhand` with a non-existent `chhand_type_id`' do
         chhand = Chhand.new(:chhand_type_id => 999, :chapter => chapter, :vaak => 'Example vaak')
         expect(chhand).not_to be_valid
       end
 
-      it 'does not save the chhand with a non-existent chapter' do
+      it 'does not save the `Chhand` with a non-existent `chapter_id`' do
         chhand = Chhand.new(:chhand_type => chhand_type, :chapter_id => 999, :vaak => 'Example vaak')
         expect(chhand).not_to be_valid
       end
 
-      it 'does not save the chhand with a null chhand_type' do
+      it 'does not save the `Chhand` with a null `chhand_type`' do
         chhand = Chhand.new(:chhand_type => nil, :chapter => chapter, :vaak => 'Example vaak')
         expect(chhand).not_to be_valid
       end
 
-      it 'does not save the chhand with a null chapter' do
+      it 'does not save the `Chhand` with a null `chapter`' do
         chhand = Chhand.new(:chhand_type => chhand_type, :chapter => nil, :vaak => 'Example vaak')
         expect(chhand).not_to be_valid
       end
