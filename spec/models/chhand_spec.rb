3dc980a36c0a3207345488b1f3d80148972592f4
Dilraj Somel <dsomel21@gmail.com>
2023-04-08 17:31:31 -0400

    Updated specs

--- spec/models/chhand_spec.rb
+++ spec/models/chhand_spec.rb
@@ -9,44 +9,50 @@ RSpec.describe Chhand do
   describe 'validations' do
     context 'when given valid attributes' do
       it 'saves the `Chhand`' do
-        chhand = Chhand.new(:chhand_type => chhand_type, :chapter => chapter, :vaak => 'Example vaak')
+        chhand = Chhand.new(:chhand_type => chhand_type, :chapter => chapter, :vaak => 'Example vaak', :sequence => 1)
         expect(chhand).to be_valid
       end
 
       it 'saves the `Chhand` without `vaak`' do
-        chhand = Chhand.new(:chhand_type => chhand_type, :chapter => chapter)
+        chhand = Chhand.new(:chhand_type => chhand_type, :chapter => chapter, :sequence => 1)
         expect(chhand).to be_valid
       end
     end
 
     context 'when given invalid attributes' do
       it 'does not save the `Chhand` without a `chhand_type`' do
-        chhand = Chhand.new(:chapter => chapter, :vaak => 'Example vaak')
+        chhand = Chhand.new(:chapter => chapter, :vaak => 'Example vaak', :sequence => 1)
         expect(chhand).not_to be_valid
       end
 
       it 'does not save the `Chhand` without a `chapter`' do
-        chhand = Chhand.new(:chhand_type => chhand_type, :vaak => 'Example vaak')
+        chhand = Chhand.new(:chhand_type => chhand_type, :vaak => 'Example vaak', :sequence => 1)
         expect(chhand).not_to be_valid
       end
 
       it 'does not save the `Chhand` with a non-existent `chhand_type_id`' do
-        chhand = Chhand.new(:chhand_type_id => 999, :chapter => chapter, :vaak => 'Example vaak')
+        chhand = Chhand.new(:chhand_type_id => 999, :chapter => chapter, :vaak => 'Example vaak', :sequence => 1)
         expect(chhand).not_to be_valid
       end
 
       it 'does not save the `Chhand` with a non-existent `chapter_id`' do
-        chhand = Chhand.new(:chhand_type => chhand_type, :chapter_id => 999, :vaak => 'Example vaak')
+        chhand = Chhand.new(:chhand_type => chhand_type, :chapter_id => 999, :vaak => 'Example vaak', :sequence => 1)
         expect(chhand).not_to be_valid
       end
 
       it 'does not save the `Chhand` with a null `chhand_type`' do
-        chhand = Chhand.new(:chhand_type => nil, :chapter => chapter, :vaak => 'Example vaak')
+        chhand = Chhand.new(:chhand_type => nil, :chapter => chapter, :vaak => 'Example vaak', :sequence => 1)
         expect(chhand).not_to be_valid
       end
 
       it 'does not save the `Chhand` with a null `chapter`' do
-        chhand = Chhand.new(:chhand_type => chhand_type, :chapter => nil, :vaak => 'Example vaak')
+        chhand = Chhand.new(:chhand_type => chhand_type, :chapter => nil, :vaak => 'Example vaak', :sequence => 1)
+        expect(chhand).not_to be_valid
+      end
+
+      it 'does not save the `Chhand` with a duplicate `sequence` within the same `chapter`' do
+        create(:chhand, :chapter => chapter, :sequence => 1)
+        chhand = Chhand.new(:chhand_type => chhand_type, :chapter => chapter, :vaak => 'Example vaak', :sequence => 1)
         expect(chhand).not_to be_valid
       end
     end
