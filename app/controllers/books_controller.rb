fe8914a289a70cb46ea67c01cd97bacbb58b69ec
anhadpreetsingh <anhadpreetsingh@gmail.com>
2023-04-25 20:40:37 +0530

    Fixed rubocop errors

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,13 @@
-class BooksController < ApplicationController
+# frozen_string_literal: true
 
+class BooksController < ApplicationController
   def index
     @books = Book.all
     render :json => @books
   end
+
   def show
     @book = Book.find(params[:id])
     render :json => @book
   end
-
 end
