7080eef23ee01f0d9237b3946ee685c8dc47ff8e
Dilraj Somel <dsomel21@gmail.com>
2023-05-08 08:52:18 -0400

    Create `pauri_footnotes` table migration

---
+++ db/migrate/20230508124727_create_pauri_footnotes.rb
@@ -0,0 +1,11 @@
+class CreatePauriFootnotes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pauri_footnotes do |t|
+      t.references :pauri, :foreign_key => true, :null => false, :index => true
+      t.text :bhai_vir_singh_footnote
+      t.string :contentful_entry_id, :index => { :unique => true }
+
+      t.timestamps
+    end
+  end
+end
