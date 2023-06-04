7976e6e3f8a6463426f6c5d1ee927f9f4d91af1c
Dilraj Somel <dsomel21@gmail.com>
2023-06-04 15:33:44 -0400

    JSON API FIX

--- config/application.rb
+++ config/application.rb
@@ -27,11 +27,11 @@ module Spg2
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000'
+        origins 'https://spg.dev/', 'localhost:3000', 'localhost:3001'
 
         resource '*',
           headers: :any,
-          methods: [:get]
+        methods: [:get]
       end
     end
 
