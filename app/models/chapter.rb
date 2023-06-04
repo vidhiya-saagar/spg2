77a228462ca1ce7a17aac5b2ae0ca035e0bda9d0
Dilraj Somel <dsomel21@gmail.com>
2023-06-04 17:44:24 -0400

    Linting

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -5,6 +5,6 @@ class Chapter < ApplicationRecord
   has_many :chhands, :dependent => :destroy
   has_many :pauris, :dependent => :destroy
   has_many :tuks, :through => :pauris
-  has_many :chapter_kathas
+  has_many :chapter_kathas, :dependent => :destroy
   has_many :kathas, :through => :chapter_kathas
 end
