2cc5d783f201f0625d86fc40c80450fbf6f449d8
Dilraj Somel <dsomel21@gmail.com>
2023-06-12 00:18:38 -0400

    Lint

--- db/migrate/20230611172633_add_soundcloud_playlist_support_to_katha.rb
+++ db/migrate/20230611172633_add_soundcloud_playlist_support_to_katha.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddSoundcloudPlaylistSupportToKatha < ActiveRecord::Migration[7.0]
   def change
     add_column :kathas, :is_playlist, :boolean, :default => false
--- db/migrate/20230611191536_remove_null_constraint_from_giani_id_on_kathas.rb
+++ db/migrate/20230611191536_remove_null_constraint_from_giani_id_on_kathas.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveNullConstraintFromGianiIdOnKathas < ActiveRecord::Migration[7.0]
   def change
     change_column_null :kathas, :giani_id, true
